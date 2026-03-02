8648d8d33c09a6ef9c6cd6f3a0eab54589dc337c
syawalqi <galihmaulanasyawalqi@gmail.com>
2026-03-02 09:50:52 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/Pertemuan2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pertemuan2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pertemuan2Application.class, args);
+	}
+
+}
